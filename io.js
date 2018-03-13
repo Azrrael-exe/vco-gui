97c43d0be0f98b2708bd571ba1b014414ffc4b3b
Azrrael-exe <vcamargo.e@gmail.com>
2018-03-13 18:38:03 -0500

    vco list

--- io.js
+++ io.js
@@ -1,4 +1,5 @@
 var io = require('socket.io')();
+const fs = require('fs');
 
 var SerialPort = require('serialport');
 var port = new SerialPort('/dev/ttyUSB0',{
@@ -8,16 +9,32 @@ var port = new SerialPort('/dev/ttyUSB0',{
 io.serveClient(false);
 io.on('connection', function(socket){
   console.log("Client with id" + socket.id + " is Connected.")
+  var raw_file = fs.readFile('./data/vco_list.json', "utf8", function(err ,raw){
+    if(raw){
+      var raw_vcos = raw.split('\n').slice(0, -1);
+      var vcos = []
+      raw_vcos.forEach(function(vco){
+        vcos.push(JSON.parse(vco))
+      })
+      socket.emit('vcos', vcos)
+    }
+  })
   socket.on('disconnect', function(){
     console.log({'user disconnected': socket.id});
   });
   socket.on('control', function(msg){
     port.write(JSON.stringify(msg), function(err){
-    if (err) {
-      return console.log('Error on write: ', err.message);
-    }
-    console.log({'Serial-OUT':JSON.stringify(msg)});
-  });
+      if (err) {
+        return console.log('Error on write: ', err.message);
+      }
+      console.log({'Serial-OUT' : JSON.stringify(msg)});
+    });
+  })
+  socket.on('save', function(msg){
+    fs.appendFile('./data/vco_list.json', JSON.stringify(msg) + '\n', function (err) {
+      if (err) throw err;
+    });
+    console.log({"Write-File" : JSON.stringify(msg)})
   })
 });
 
--- public/app/app.js
+++ public/app/app.js
@@ -9,12 +9,31 @@ app.factory('socket', function (socketFactory) {
 });
 
 app.controller("voltageController", function($scope, socket) {
-  $scope.sendControl = function(amp, off, noi) {
-    var control = {
-      amplitude : amp,
-      offset : off,
-      noise : noi
+  socket.on('vcos', function(msg){
+    $scope.vcos = msg
+  })
+  $scope.sendControl = function(id, amp, off, noi) {
+    var control = {}
+    control[id] = {
+      A : amp,
+      O : off,
+      N : noi
     }
     socket.emit('control', control)
+  }
+  $scope.saveConfig = function(name, vco, amp, off, noi){
+    var config = {
+      name : name,
+      vco : vco,
+      A : amp,
+      O : off,
+      N : noi
+    }
+    socket.emit('save', config)
+  }
+  $scope.setConfig = function(vco){
+    $scope.amp = vco.A
+    $scope.off = vco.O
+    $scope.noi = vco.N
   }
 })
